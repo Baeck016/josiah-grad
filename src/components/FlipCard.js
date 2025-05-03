1ba963184dcf1d6d666b8f022eea2f98200eb353
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2025-05-04 00:21:09 +0800

    change to tap

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -1,40 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Text, VStack } from "@chakra-ui/react";
 import "./flipcard.css";
 
-const FlipCard = () => {
-  const Card = (frontimage, backimage) => {
-    return (
-      <Box p={4}>
-        <Box className="flip-card" mx="auto">
-          <Box className="flip-card-inner">
-            <Box className="flip-card-front">
-              <Box
-                bgImage={`url(${process.env.PUBLIC_URL + frontimage})`}
-                bgSize="cover"
-                width="100%"
-                height="100%"
-              />
-            </Box>
+const Card = ({ frontimage, backimage }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <Box p={4} onClick={handleFlip}>
+      <Box className="flip-card" mx="auto">
+        <Box className={`flip-card-inner ${isFlipped ? "flipped" : ""}`}>
+          <Box className="flip-card-front">
             <Box
-              className="flip-card-back"
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-            >
-              <Box
-                bgImage={`url(${process.env.PUBLIC_URL + backimage})`}
-                bgSize="cover"
-                width="100%"
-                height="100%"
-              />
-            </Box>
+              bgImage={`url(${process.env.PUBLIC_URL + frontimage})`}
+              bgSize="cover"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+          <Box
+            className="flip-card-back"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Box
+              bgImage={`url(${process.env.PUBLIC_URL + backimage})`}
+              bgSize="cover"
+              width="100%"
+              height="100%"
+            />
           </Box>
         </Box>
       </Box>
-    );
-  };
+    </Box>
+  );
+};
 
+const FlipCard = () => {
   return (
     <VStack
       id="main-container"
@@ -43,17 +50,20 @@ const FlipCard = () => {
       justifyContent="center"
       gap="4"
     >
-      {Card("images/front-page.png", "images/back-page.png")}
-      {Card("images/4.png", "images/5.png")}
-      {Card("images/6.png", "images/7.png")}
-      {Card("images/8.png", "images/9.png")}
-      {Card("images/10.png", "images/11.png")}
-      {Card("images/12.png", "images/13.png")}
-      {Card("images/14.png", "images/15.png")}
-      {Card("images/16.png", "images/17.png")}
-      {Card("images/18.png", "images/19.png")}
-      {Card("images/20.png", "images/21.png")}
-      {Card("images/22.png", "images/23.png")}
+      <Card
+        frontimage="images/front-page.png"
+        backimage="images/back-page.png"
+      />
+      <Card frontimage="images/4.png" backimage="images/5.png" />
+      <Card frontimage="images/6.png" backimage="images/7.png" />
+      <Card frontimage="images/8.png" backimage="images/9.png" />
+      <Card frontimage="images/10.png" backimage="images/11.png" />
+      <Card frontimage="images/12.png" backimage="images/13.png" />
+      <Card frontimage="images/14.png" backimage="images/15.png" />
+      <Card frontimage="images/16.png" backimage="images/17.png" />
+      <Card frontimage="images/18.png" backimage="images/19.png" />
+      <Card frontimage="images/20.png" backimage="images/21.png" />
+      <Card frontimage="images/22.png" backimage="images/23.png" />
       <Text
         textColor="white"
         textAlign="center"
