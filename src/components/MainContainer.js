ba4b4386ff0e0df73b7cdd04ffec36eea53537c3
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2025-05-03 22:34:41 +0800

    changed icon and readme

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, chakra, Image } from "@chakra-ui/react";
+
+const MainContainer = () => {
+  return (
+    <chakra.main minH="100%" minW="100%" flex={1} id="main-container">
+      <Box>HI</Box>
+    </chakra.main>
+  );
+};
+
+export default MainContainer;
